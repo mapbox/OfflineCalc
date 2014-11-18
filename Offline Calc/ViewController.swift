403391a890afcd59618ff75c50be15eb4bd88e6f
Justin R. Miller <incanus@codesorcery.net>
2014-11-18 10:34:05 -0800

    cleanups & offline calc

--- Offline Calc/ViewController.swift
+++ Offline Calc/ViewController.swift
@@ -8,16 +8,24 @@ class ViewController: UIViewController, RMMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "Offline Calc"
-        self.navigationController?.toolbarHidden = false
+        title = "Offline Calc"
+        navigationController?.toolbarHidden = false
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Organize,
+                                                            target: self,
+                                                            action: "calculateOffline")
 
         centerLabel = UIBarButtonItem(title: "x, x, x", style: .Plain, target: nil, action: nil)
         centerLabel?.enabled = false
 
         if (centerLabel != nil) {
-            self.toolbarItems = [ UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil),
-                centerLabel!,
-                UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil) ]
+            toolbarItems = [ UIBarButtonItem(barButtonSystemItem: .FlexibleSpace,
+                                             target: nil,
+                                             action: nil),
+                             centerLabel!,
+                             UIBarButtonItem(barButtonSystemItem: .FlexibleSpace,
+                                             target: nil,
+                                             action: nil) ]
         }
 
         map = RMMapView(frame: view.bounds)
@@ -25,9 +33,43 @@ class ViewController: UIViewController, RMMapViewDelegate {
         if (map != nil) {
             map!.delegate = self
             view.addSubview(map!)
-            self.mapViewRegionDidChange(map)
+            mapViewRegionDidChange(map)
         }
 
+    }
+
+    func calculateOffline() {
+        let bbox = map!.latitudeLongitudeBoundingBox()
+
+        var output = "\n"
+
+        let lowZoom = UInt(floor(map!.zoom))
+        let highZoom:UInt = 20
+
+        var total: UInt = 0
+
+        for zoom in lowZoom...highZoom {
+            let zoomCount = map!.tileCache.tileCountForSouthWest(bbox.southWest,
+                                                                 northEast: bbox.northEast,
+                                                                 minZoom: lowZoom,
+                                                                 maxZoom: zoom)
+            total += zoomCount
+            output += "z\(zoom): \(zoomCount) tiles\n"
+        }
+
+        let alert = UIAlertController(title: "Tile Counts",
+                                      message: output,
+                                      preferredStyle: .Alert)
+
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .Default,
+                                      handler: { [unowned self] (action) in
+                                          self.dismissViewControllerAnimated(true, completion: nil)
+                                      }))
+
+        presentViewController(alert,
+                              animated: true,
+                              completion: nil)
     }
 
     func mapViewRegionDidChange(mapView: RMMapView!) {

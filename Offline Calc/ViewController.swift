5d0bcdfca0e73ce21b5ca6c3c526de7cc03ff2b3
Justin R. Miller <incanus@codesorcery.net>
2014-11-18 09:38:00 -0800

    basic Mapbox SDK setup

--- Offline Calc/ViewController.swift
+++ Offline Calc/ViewController.swift
@@ -1,25 +1,11 @@
-//
-//  ViewController.swift
-//  Offline Calc
-//
-//  Created by Justin R. Miller on 11/18/14.
-//  Copyright (c) 2014 Mapbox. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addSubview(RMMapView(frame: view.bounds))
     }
-
 
 }
-
